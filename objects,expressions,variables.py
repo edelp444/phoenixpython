4ef780c0ed2f06c38942f9c5dbf9815f7eb1c353
edelp444 <71664636+edelp444@users.noreply.github.com>
2020-09-21 14:20:55 -0400

    Add files via upload

---
+++ objects,expressions,variables.py
@@ -0,0 +1,16 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> type(6//2)
+<class 'int'>
+>>> 160/60
+2.6666666666666665
+>>> x=3+2*2
+>>> x
+7
+>>> y=(3+2)*2
+>>> y
+10
+>>> z=x+y
+>>> z
+17
+>>> 
