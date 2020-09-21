0f7f7216d7a72fe5e125deb886e05a59f18c3847
edelp444 <71664636+edelp444@users.noreply.github.com>
2020-09-21 15:12:10 -0400

    Add files via upload

---
+++ slicingstridesupper.py
@@ -0,0 +1,15 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Letters=("ABCDEFGHIJK")
+>>> Letters[0:3]
+'ABC'
+>>> Letters[0:5]
+'ABCDE'
+>>> Good="GsoAo+d"
+>>> Good[::2]
+'Good'
+>>> a=("uppercaseone")
+>>> b=a.upper()
+>>> b
+'UPPERCASEONE'
+>>> 
